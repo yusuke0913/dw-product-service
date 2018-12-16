e7184d1dfcd03f2cb40ae390c39fdee5c1ec66c1
Ke_Giang <yusuke.social.ebisu@gmail.com>
2018-12-16 09:50:41 +0100

    Renamed Models to Model

--- product-service/app/Http/Controllers/Api/V1/ProductController.php
+++ product-service/app/Http/Controllers/Api/V1/ProductController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\Product;
+use App\Model\Product;
 
 class ProductController extends Controller
 {
